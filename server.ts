f8082b81ef7e223816cedaa761f615ac26a6b50c
malayatr941 <malay@antiersolutions.com>
2023-10-25 16:56:18 +0530

    removed unused code

---
+++ server.ts
@@ -0,0 +1,14 @@
+import * as config from './config';
+import {connect} from './src/connection';
+import UserController from './src/controllers/user.controller';
+
+(async () => {
+   await config.initiate();
+})();
+import App from './src/app';
+
+const app = new App([new UserController()]);
+
+connect();
+
+app.listen();
--- src/validation/user.validation.ts
+++ src/validation/user.validation.ts
@@ -10,7 +10,7 @@ export const registerValidation = (
     name: Joi.string().min(3).required(),
     password: Joi.string().min(8).required(),
     email: Joi.string().email().required(),
-    company: Joi.string().domain().required()
+    company: Joi.string().required()
   });
 
   const { error } = schema.validate(req.body);
